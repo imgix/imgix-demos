0fe82765af9bd49416765a91dad338320027ab1b
Tom Dale <thomas.dale2@gmail.com>
2022-08-18 21:56:30 -0700

    Update post-header.tsx

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -140,9 +140,7 @@ const PostHeader = (props: PostHeaderProps) => {
                   <Link
                     href="https://next-merge-six.vercel.app/"
                     className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                  >
-                    See Other Demos
-                  </Link>
+                  ><a>See Other Demos</a></Link>
                 </div>
               </div>
             </div>
