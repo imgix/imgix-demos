2dc9e5350640dca1ce8071188b52557bcbea9005
Tom Dale <thomas.dale2@gmail.com>
2022-09-01 11:28:42 -0700

    update favicons

--- components/intro.tsx
+++ components/intro.tsx
@@ -133,7 +133,7 @@ const Intro = () => {
                     href="https://next-merge-tomdale.vercel.app/"
                     className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
-                    See Demos
+                    See All Demos
                   </a>
                 </div>
               </div>
