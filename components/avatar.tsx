30ade6461dab0f2fd6fe0283de5d134d749d9873
Tom Dale <tom@imgix.com>
2022-10-24 22:11:32 -0700

    correct avatar sizing

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -10,8 +10,8 @@ const Avatar = (props: AvatarProps) => {
       {picture && (
         // eslint-disable-next-line @next/next/no-img-element
         <img
-          src={`${picture}?auto=format,compress,enhance&w=150&h=150&fit=facearea&facepad=2.5`}
-          srcSet={`${picture}?auto=format,compress,enhance&w=150&h=150&fit=facearea&facepad=2.5 1x, ${picture}?auto=format,compress,enhance&w=150&h=150&fit=facearea&facepad=2.5&dpr=2 2x, ${picture}?auto=format,compress,enhance&w=150&h=150&fit=facearea&facepad=2.5&dpr=3 3x`}
+          src={`${picture}?auto=format,compress,enhance&w=50&h=50&fit=facearea&facepad=2.5`}
+          srcSet={`${picture}?auto=format,enhance&w=50&h=50&fit=facearea&facepad=2.5 1x, ${picture}?auto=format,compress,enhance&w=50&h=50&fit=facearea&facepad=2.5&dpr=2 2x, ${picture}?auto=format,compress,enhance&w=50&h=50&fit=facearea&facepad=2.5&dpr=3 3x`}
           className="w-12 h-12 mr-4 rounded-full"
           alt={name}
         />
